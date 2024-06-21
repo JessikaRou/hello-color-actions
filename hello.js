bf0a7e1ec67f260889d721a794c06a141b4a0679
JessikaRou <jessikaroussy@modechoc.ca>
2024-06-21 11:24:40 -0400

    firstcommit

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Quebec lac saint jean!";
 console.log(hello());
 exports.hello = hello;
 
